8118c5cac1e5dc4f6dd7de0a001d37230d1ef714
Damien <dtjldamien@gmail.com>
2019-03-23 17:35:13 +0800

    Make model for Childcare Centre

--- website/website/settings.py
+++ website/website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'locate.apps.LocateConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
