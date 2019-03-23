44644adf5bef368ceb54faa9851da557216ab0b8
Damien <dtjldamien@gmail.com>
2019-03-23 17:42:52 +0800

    Updated Childcare models

--- website/locate/models.py
+++ website/locate/models.py
@@ -5,3 +5,6 @@ from django.db import models
 
 class ChildcareCentre(models.Model):
     name = models.CharField(max_length=250)
+    fees = models.FloatField(max_length=10000)
+    infantVacancies = models.CharField(max_length=100)
+    k2Vacancies = models.CharField(max_length=100)
